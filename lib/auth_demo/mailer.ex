eadc0387684525f175296912da1c00bba4e06a2b
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2023-10-06 22:19:07 +0530

    - Removed all data

--- lib/auth_demo/mailer.ex
+++
@@ -1,3 +0,0 @@
-defmodule AuthDemo.Mailer do
-  use Swoosh.Mailer, otp_app: :auth_demo
-end
--- lib/auth_demo/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule AuthDemo.Repo do
-  use Ecto.Repo,
-    otp_app: :auth_demo,
-    adapter: Ecto.Adapters.Postgres
-end
